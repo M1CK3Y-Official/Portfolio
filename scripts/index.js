315d11069e35841dcf8c32770358b3974c231d99
[Mike <[mikeboldsen2004@gmail.com]>
2023-02-11 00:41:58 +0100

    going to bed now xd

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const item = document.querySelector('.item');
+item.addEventListener('mouseover', () => {
+    const test = document.querySelector('.test');
+    test.innerHTML = `<h2>Cryptana Invest</h2>`;
+})
